b5e8817145d3c6fb181d4de5f72528e2dd06389b
oliver <oliver.koping@outlook.com>
2020-09-23 21:31:40 +0200

    set up db file, fixed connection auth

---
+++ ReactorSafetySystem_Jokers/app/src/main/java/com/example/reactorsafetysystem_jokers/DatabaseRFIDRepository.java
@@ -0,0 +1,31 @@
+package com.example.reactorsafetysystem_jokers;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+public class DatabaseRFIDRepository {
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+
+    public Task<QuerySnapshot> getUserInfo(String userId) {
+
+        Log.d("yeeeeeeeeeeeeeeeeeeeeet",userId);
+
+        return db.collection("users").whereEqualTo("userId",userId).get();
+
+
+
+    }
+
+
+
+
+}
--- ReactorSafetySystem_Jokers/app/src/main/java/com/example/reactorsafetysystem_jokers/MainActivity.java
+++ ReactorSafetySystem_Jokers/app/src/main/java/com/example/reactorsafetysystem_jokers/MainActivity.java
@@ -1,17 +1,91 @@
 package com.example.reactorsafetysystem_jokers;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    DatabaseRFIDRepository db = new DatabaseRFIDRepository();
+
+    private static final int RC_SIGN_IN = 123;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //createSignInIntent();
+
+        db.getUserInfo("qpGCZ9QCMdh4AfwheTy7ShUNF").addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        Log.d("yeet", document.getId() + " => " + document.getData());
+                    }
+                } else {
+                    Log.w("yeet", "Error getting documents.", task.getException());
+                }
+            }
+        });
+
+
     }
 
+    public void createSignInIntent() {
+        // [START auth_fui_create_intent]
+        // Choose authentication providers
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build());
+
+        // Create and launch sign-in intent
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN);
+        // [END auth_fui_create_intent]
+    }
+
+    // [START auth_fui_result]
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                // ...
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
+    }
+    // [END auth_fui_result]
 
 /*
 
